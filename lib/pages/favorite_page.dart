b7d8a36c76b797f91db13c03d9457dc40e43bff0
KiberYebok2009 <126006305+KiberYebok2009@users.noreply.github.com>
2024-10-09 15:19:29 +0300

    Add files via upload

---
+++ lib/pages/favorite_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/components/fav_item_note.dart';
+
+class FavoritePage extends StatefulWidget {
+  const FavoritePage({super.key,});
+
+  @override
+  State<FavoritePage> createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
+
+  void _removeTovar(int index) {
+    setState(() {
+      favorite.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(favorite.isEmpty){
+      return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('ИЗБРАННОЕ', style: TextStyle(fontSize: 24, color: Colors.white),),), backgroundColor: Colors.black,
+      ),
+      backgroundColor:  const Color.fromARGB(255, 139, 147, 255),
+      body: Padding(padding: const EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('../2.webp', width: 300, height: 300,),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Ничего пока что нет', style: TextStyle(fontSize: 32, color: Colors.white),
+              ),
+            )
+          ],
+          ),
+        ),
+      ),
+    );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('ИЗБРАННОЕ', style: TextStyle(fontSize: 24, color: Colors.white),),), backgroundColor: Colors.black,
+      ),
+      backgroundColor:  const Color.fromARGB(255, 139, 147, 255),
+      body: Padding(padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+        padding: const EdgeInsets.all(8),
+          itemCount: favorite.length,
+          itemBuilder: (BuildContext context, int index) {
+            return FavItemNote(
+              tovar: favorite[index], 
+              onRemove: () => _removeTovar(index),
+            );
+          },
+        ),
+      ),
+
+    );
+  }
+}
---
+++ lib/pages/note_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/note.dart';
+
+class NotePage extends StatelessWidget {
+  const NotePage({super.key, required this.tovar, required this.onRemove, required this.onFavorite,});
+
+  final Tovar tovar;
+  final VoidCallback onRemove;
+  final VoidCallback onFavorite;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text('АМНЯМ', style: TextStyle(fontSize: 24, color: Colors.white),),), backgroundColor: Colors.black,
+      ),
+      body: 
+        Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.7,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255,255, 113, 205), borderRadius: BorderRadius.circular(16), border: Border.all(color: Colors.white, width: 2), 
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(tovar.discription, style: const TextStyle(fontSize: 36, color: Colors.white),),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(child: Image.asset(tovar.url, height: 200, width: 200,)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text('Цена: ${tovar.price}', style: const TextStyle(fontSize: 36, color: Colors.white),),
+                ),
+                Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 139, 147, 255),),
+                    onPressed: onRemove,
+                    child: const Text('Удалить товар', style: TextStyle(fontSize: 20, color: Colors.white,),),),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                    ),
+                    backgroundColor: const Color.fromARGB(255, 139, 147, 255),),
+                    onPressed: onFavorite,
+                    child: const Text('В избранное', style: TextStyle(fontSize: 20, color: Colors.white,),),),
+                ),
+              ),
+              ],
+            ),
+          ),
+        ), backgroundColor: const Color.fromARGB(255, 139, 147, 255),
+    );
+  }
+}
